5d610e46671b4ace2fb98c5238fa30783cc9379f
Krishna Panthi <krishnapantheee@gmail.com>
2026-02-01 12:17:45 +0545

    refactor: changed hero-image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,7 +40,7 @@ export default function Home() {
         {/* Background Image */}
         <div className="absolute inset-0 -z-10">
           <img
-            src="https://images.unsplash.com/photo-1523240795612-9a054b0db644?auto=format&fit=crop&w=2000&q=80"
+            src="/saipal-hero.jpeg"
             alt="Outdoor university campus shot"
             className="h-full w-full object-cover brightness-105 contrast-105"
           />
